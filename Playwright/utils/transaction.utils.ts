399c5ac15045de958d7f7137ea4cd8aa672b1e64
Tomaszlek <tjarmoc1@wp.pl>
2026-03-26 19:40:24 +0100

    Dorzucenie mojej paczki

--- Playwright/utils/transaction.utils.ts
+++ Playwright/utils/transaction.utils.ts
@@ -2,7 +2,7 @@ import { Page, Locator } from '@playwright/test';
 
 /**
  * Klika przycisk "Kup" dla wybranego samochodu zdefiniowanego przez selektor
- * @param page 
+ * @param page
  * @param carLocator - selektor konkretnej karty (.card)
  */
 export async function buyCar(page: Page, carLocator: Locator): Promise<void> {
@@ -10,8 +10,16 @@ export async function buyCar(page: Page, carLocator: Locator): Promise<void> {
 }
 
 /**
+ * Klika przycisk "Kup" na stronie szczegółów samochodu
+ * @param page
+ */
+export async function buyCarFromDetail(page: Page): Promise<void> {
+  await page.getByRole('button', { name: 'Kup' }).click();
+}
+
+/**
  * Klika przycisk "Wypożycz" dla wybranego samochodu
- * @param page 
+ * @param page
  * @param carLocator - selektor karty pojazdu (.card)
  */
 export async function rentCar(page: Page, carLocator: Locator): Promise<void> {
